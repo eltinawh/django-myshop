77b6ef370d0ddfac713f8469551631afab352091
eltinawh <eltinawh@yahoo.com>
2025-02-07 15:56:23 +0700

    Try django localflavor for form field validation based on local

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from localflavor.us.forms import USZipCodeField
 from orders.models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    # postal_code = USZipCodeField()
     class Meta:
         model = Order
         fields = [
